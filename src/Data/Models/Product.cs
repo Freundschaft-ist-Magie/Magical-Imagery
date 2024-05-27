1ae25c9cf03e28c919a84708ed7eb1f5731bf48d
Justin <justin-urbanek@bluewin.ch>
2024-05-27 11:27:29 +0200

    style: product formating violation fix

--- src/Data/Models/Product.cs
+++ src/Data/Models/Product.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Data.Models;
 
-namespace Data.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-        public uint Price { get; set; }
-        public string? Category { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public string Pixel => $"{Width}X{Height}";
-        public int LicenceId { get; set; }
-        public Licence? Licence { get; set; }
-    }
-}
+  public class Product
+  {
+      public int Id { get; set; }
+      public string Name { get; set; } = string.Empty;
+      public string? Description { get; set; }
+      public uint Price { get; set; }
+      public string? Category { get; set; }
+      public int Width { get; set; }
+      public int Height { get; set; }
+      public string Pixel => $"{Width}X{Height}";
+      public int LicenceId { get; set; }
+      public Licence? Licence { get; set; }
+  }
