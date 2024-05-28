5956671e01b4380fc8d95f12407d0595465f46fb
Justin <justin-urbanek@bluewin.ch>
2024-05-28 15:43:37 +0200

    feat: Licence Description props

--- src/Data/Models/Licence.cs
+++ src/Data/Models/Licence.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Data.Models;
 
-namespace Data.Models
-{
-    public class Licence
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public List<Product>? Products { get; set; }
-    }
-}
+  public class Licence
+  {
+      public int Id { get; set; }
+      public string Name { get; set; } = string.Empty;
+      public string Description { get; set; } = string.Empty;
+      public string ShortDescription { get; set; } = string.Empty;
+      public List<Product>? Products { get; set; }
+  }
